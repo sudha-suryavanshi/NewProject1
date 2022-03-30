0099bc3e763f0cc1d04b29d93b9da4539a54a9db
sudha-suryavanshi <sudha.suryavanshi@gmail.com>
2022-03-31 00:57:26 +0530

    done

---
+++ Project123/src/add.java
@@ -0,0 +1,13 @@
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int num1,num2,sum;
+        num1=10; num2=20;
+        sum=num1+num2;
+     System.out.println("Hello");
+     System.out.println(sum); 
+	}
+
+}
